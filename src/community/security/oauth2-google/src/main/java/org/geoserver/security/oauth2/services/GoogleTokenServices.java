8083e5f9980bc42d4e63a962da5f94d46df8dda8
Denis <d.alekseev@mycrg.ru>
2018-12-26 23:12:52 +0300

    map user_name -> client_id

--- src/community/security/oauth2-google/src/main/java/org/geoserver/security/oauth2/services/GoogleTokenServices.java
+++ src/community/security/oauth2-google/src/main/java/org/geoserver/security/oauth2/services/GoogleTokenServices.java
@@ -6,10 +6,8 @@ package org.geoserver.security.oauth2.services;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
-
 import java.nio.charset.StandardCharsets;
 import java.util.Map;
-
 import org.geoserver.security.oauth2.GeoServerOAuthRemoteTokenServices;
 import org.springframework.security.crypto.codec.Base64;
 
@@ -25,10 +23,11 @@ public class GoogleTokenServices extends GeoServerOAuthRemoteTokenServices {
     }
 
     protected Map<String, Object> checkToken(String accessToken) {
-        Claims claims = Jwts.parser()
-                .setSigningKey(clientSecret.getBytes())
-                .parseClaimsJws(accessToken)
-                .getBody();
+        Claims claims =
+                Jwts.parser()
+                    .setSigningKey(clientSecret.getBytes())
+                    .parseClaimsJws(accessToken)
+                    .getBody();
 
         LOGGER.warn("Fiz claims: {}", claims);
         return claims;
@@ -36,8 +35,9 @@ public class GoogleTokenServices extends GeoServerOAuthRemoteTokenServices {
 
     protected void transformNonStandardValuesToStandardValues(Map<String, Object> map) {
         LOGGER.debug("Original map = " + map);
-//        map.put("client_id", map.get("issued_to")); // Google sends 'client_id' as 'issued_to'
-//        map.put("user_name", map.get("user_id")); // Google sends 'user_name' as 'user_id'
+        map.put("client_id", map.get("user_name")); // Google sends 'client_id' as
+        // 'issued_to'
+        //        map.put("user_name", map.get("user_id")); // Google sends 'user_name' as 'user_id'
         LOGGER.debug("Transformed = " + map);
     }
 
